e7cb578e73543f47e1683e26601b9b5d87fc2f38
paco <paco@pacos-MacBook-Pro.local>
2021-11-10 11:52:04 -0800

    add alert

---
+++ alert.py
@@ -0,0 +1,13 @@
+class Alert:
+    def __init__(self,server,message,severity):
+        self.server = server
+        self.message = message
+        self.severity = severity
+
+alerts = []
+alerts.append(Alert("charlie","ate chocolate","critical"))
+alerts.append(Alert("grandpa","fell out of bed", "critical"))
+alerts.append(Alert("oompa","stubbed toe","critical"))
+alerts.append(Alert("charlie","lost ticket","critical"))
+alerts.append(Alert("oompa","called in sick","warn"))
+alerts.append(Alert("charlie","san a song","warn"))
